504470103f06ba4b3c3cf92b996dc2ab74fe267b
Muhammad Rehan <rehanbhaifromsdk@gmail.com>
2026-02-11 16:51:21 +0500

    Initial commit

---
+++ resources/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz System</title>
+</head>
+<style>
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    background: #f2f2f2;
+    font-family: 'Poppins', sans-serif;
+}
+h1{
+    text-align: center;
+    margin: 20px 0;
+}
+</style>
+<body>
+    <h1>Welcome to the Quiz System</h1>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+route::get('damo', function () {
+    return view('index');
+});
+
+?>
